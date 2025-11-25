fe7041def7174bc511228540120e8d5b0b7caf44
caesar127 <takondwacaesarndawala@gmail.com>
2025-11-25 23:49:04 +0200

    feat(store): add category and organizer events reducers

--- web/src/store.js
+++ web/src/store.js
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "./features/auth/authSlice";
 import eventReducer from "./features/events/eventsSlice";
+import categoryReducer from "./features/category/categorySlice";
+import organizerEventsReducer from "./features/organizer-events/organizerEventsSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
     event: eventReducer,
+    category: categoryReducer,
+    organizerEvents: organizerEventsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
