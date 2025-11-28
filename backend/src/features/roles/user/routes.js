a119e540100522614bd365a95d644e2ae0c376f3
caesar127 <takondwacaesarndawala@gmail.com>
2025-11-28 14:31:35 +0200

    feat(wallet): add wallet routes

--- backend/src/features/roles/user/routes.js
+++ backend/src/features/roles/user/routes.js
@@ -3,14 +3,14 @@ import userEventsRoutes from "./userEvents.routes.js";
 import authRoutes from "../../auth/auth.routes.js";
 import userRoutes from "../../users/users.routes.js";
 import connectionRoutes from "../../connection/connection.routes.js";
+import walletRoutes from "../../wallet/wallet.routes.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
-
 router.use("/events", userEventsRoutes);
-
 router.use("/connections", connectionRoutes);
+router.use("/wallet", walletRoutes);
 
 export default router;
