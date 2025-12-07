07b95b3045e4a4166cf049131b65fcfad3ac102d
caesar127 <takondwacaesarndawala@gmail.com>
2025-12-07 12:56:28 +0200

    chore(vendor): remove vendor events routes import

--- backend/src/features/roles/vendor/routes.js
+++ backend/src/features/roles/vendor/routes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { requireRole } from "../../../core/middleware/auth.middleware.js";
 import { USER_ROLES } from "../../../core/utils/constants.js";
-import vendorEventsRoutes from "../../vendor-events/vendorEvents.routes.js";
 
 const router = express.Router();
 
