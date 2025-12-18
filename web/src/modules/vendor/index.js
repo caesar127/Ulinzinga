2922f8c8d08ad94dd26d482df4049b4c153b2689
caesar127 <takondwacaesarndawala@gmail.com>
2025-12-18 06:41:43 +0200

    refactor(vendor): reorganize vendor module exports and page structure
    - Rename ServicesPage to VendorEventsPage
    - Rename RevenuesPage to VendorAnalyticsPage
    - Update import paths to use double quotes for consistency
    - Reorganize page exports for better module structure

--- web/src/modules/vendor/index.js
+++ web/src/modules/vendor/index.js
@@ -1,14 +1,11 @@
-// Vendor module index - exports all vendor module components and routes
-export { default as VendorRoutes } from './routes';
+export { default as VendorRoutes } from "./routes";
+export { default as VendorLayout } from "./components/layout/VendorLayout";
+export { default as VendorSidebar } from "./components/layout/VendorSidebar";
+export { default as VendorHeader } from "./components/layout/VendorHeader";
 
-// Re-export layout components
-export { default as VendorLayout } from './components/layout/VendorLayout';
-export { default as VendorSidebar } from './components/layout/VendorSidebar';
-export { default as VendorHeader } from './components/layout/VendorHeader';
-
-// Re-export pages
-export { default as VendorDashboardPage } from './pages/DashboardPage';
-export { default as ServicesPage } from './pages/ServicesPage';
-export { default as BookingsPage } from './pages/BookingsPage';
-export { default as RevenuesPage } from './pages/RevenuesPage';
-export { default as VendorProfilePage } from './pages/ProfilePage';
+export { default as VendorDashboardPage } from "./pages/DashboardPage";
+export { default as VendorEventsPage } from "./pages/EventsPage";
+export { default as VendorProductsPage } from "./pages/ProductsPage";
+export { default as VendorOrdersPage } from "./pages/OrdersPage";
+export { default as VendorAnalyticsPage } from "./pages/AnalyticsPage";
+export { default as VendorProfilePage } from "./pages/ProfilePage";
