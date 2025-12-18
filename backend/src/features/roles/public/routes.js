43f85276e0b986862e84a8e1d2f33ef3c66dffb4
caesar127 <takondwacaesarndawala@gmail.com>
2025-12-18 05:48:22 +0200

    feat(routes): add qrcode routes to public router

--- backend/src/features/roles/public/routes.js
+++ backend/src/features/roles/public/routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import eventsRoutes from "../../events/events.routes.js";
 import categoryRoutes from "../../category/category.routes.js";
+import qrcodeRoutes from "../../tickets/qrcode.routes.js";
 
 const router = express.Router();
 
 router.use("/events", eventsRoutes);
 router.use("/categories", categoryRoutes);
+router.use("/qrcode", qrcodeRoutes);
 
 export default router;
