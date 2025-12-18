cf7d57249d3fc27afc42c90e37a471aea0842b2d
caesar127 <takondwacaesarndawala@gmail.com>
2025-12-18 06:40:33 +0200

    feat(organizer): add event, transaction, and stall management routes

--- web/src/modules/organizer/routes.jsx
+++ web/src/modules/organizer/routes.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
 import EventsPage from "./pages/EventsPage";
+import EventManagementPage from "./pages/EventManagementPage";
 import AttendeesPage from "./pages/AttendeesPage";
 import AnalyticsPage from "./pages/AnalyticsPage";
 import ProfilePage from "./pages/ProfilePage";
 import ProtectedRoute from "@/shared/ProtectedRoute";
 import OrganizerLayout from "./components/layout/OrganizerLayout";
+import TransactionsPage from "./pages/TransactionsPage";
+import StallsPage from "./pages/StallsPage";
 
 const OrganizerRoutes = () => {
   return (
@@ -14,6 +17,9 @@ const OrganizerRoutes = () => {
         <Route element={<OrganizerLayout />}>
           <Route path="dashboard" element={<DashboardPage />} />
           <Route path="events" element={<EventsPage />} />
+          <Route path="event" element={<EventManagementPage />} />
+          <Route path="stalls/:eventId" element={<StallsPage />} />
+          <Route path="transaction" element={<TransactionsPage />} />
           <Route path="attendees" element={<AttendeesPage />} />
           <Route path="analytics" element={<AnalyticsPage />} />
           <Route path="profile" element={<ProfilePage />} />
