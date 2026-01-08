33bbb9fe7fa2a65755fbb12a37a0a4bfdbdf2b69
caesar127 <takondwacaesarndawala@gmail.com>
2026-01-08 13:44:15 +0200

    refactor(admin): comment out unused admin page exports

--- web/src/modules/admin/index.js
+++ web/src/modules/admin/index.js
@@ -8,10 +8,10 @@ export { default as AdminHeader } from './components/layout/AdminHeader';
 
 // Re-export pages
 export { default as AdminDashboardPage } from './pages/DashboardPage';
-export { default as UsersPage } from './pages/UsersPage';
-export { default as AdminEventsPage } from './pages/EventsPage';
-export { default as VendorsPage } from './pages/VendorsPage';
-export { default as OrganizersPage } from './pages/OrganizersPage';
-export { default as AdminAnalyticsPage } from './pages/AnalyticsPage';
-export { default as SettingsPage } from './pages/SettingsPage';
-export { default as ReportsPage } from './pages/ReportsPage';
+// export { default as UsersPage } from './pages/UsersPage';
+// export { default as AdminEventsPage } from './pages/EventsPage';
+// export { default as VendorsPage } from './pages/VendorsPage';
+// export { default as OrganizersPage } from './pages/OrganizersPage';
+// export { default as AdminAnalyticsPage } from './pages/AnalyticsPage';
+// export { default as SettingsPage } from './pages/SettingsPage';
+// export { default as ReportsPage } from './pages/ReportsPage';
