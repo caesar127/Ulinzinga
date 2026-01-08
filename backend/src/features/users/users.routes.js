18b973ffc3bfb965c6418e4e0ec437e3cde78620
caesar127 <takondwacaesarndawala@gmail.com>
2026-01-08 12:36:47 +0200

    feat(api): add user picture upload route

--- backend/src/features/users/users.routes.js
+++ backend/src/features/users/users.routes.js
@@ -6,12 +6,14 @@ import {
   updateUser,
   deleteUser,
   updateUserProfile,
+  updateUserPicture,
   updateUserInterests,
   getCurrentUserProfile,
 } from "./users.controller.js";
 import Joi from "joi";
 import { validateRequest } from "../../core/utils/validators.js";
 import { verifyToken } from "../../core/middleware/auth.middleware.js";
+import { uploadSingle } from "../../core/middleware/upload.middleware.js";
 
 const router = express.Router();
 
@@ -32,6 +34,7 @@ router.get("/:id", getUserById);
 router.post("/profile/:id", getCurrentUserProfile);
 router.put("/:id", updateUser);
 router.put("/:id/profile", updateUserProfile);
+router.post("/picture", verifyToken, uploadSingle("picture"), updateUserPicture);
 router.put(
   "/:id/interests",
   validateRequest(interestsUpdateSchema),
