ab97b5c8a15018c6c1afe625b18fa6d4dd7997cb
caesar127 <takondwacaesarndawala@gmail.com>
2026-01-08 13:46:25 +0200

    refactor(organizer): reorder page exports in index file
    Reorganize page exports in the organizer module index file in alphabetical
    order for improved maintainability and consistency.

--- web/src/modules/organizer/index.js
+++ web/src/modules/organizer/index.js
@@ -7,8 +7,9 @@ export { default as OrganizerSidebar } from './components/layout/OrganizerSideba
 export { default as OrganizerHeader } from './components/layout/OrganizerHeader';
 
 // Re-export pages
-export { default as OrganizerDashboardPage } from './pages/DashboardPage';
-export { default as OrganizerEventsPage } from './pages/EventsPage';
 export { default as AttendeesPage } from './pages/AttendeesPage';
-export { default as OrganizerAnalyticsPage } from './pages/AnalyticsPage';
+export { default as OrganizerEventsPage } from './pages/EventsPage';
 export { default as OrganizerProfilePage } from './pages/ProfilePage';
+export { default as TransactionsPage } from './pages/TransactionsPage';
+export { default as OrganizerAnalyticsPage } from './pages/AnalyticsPage';
+export { default as OrganizerDashboardPage } from './pages/DashboardPage';
